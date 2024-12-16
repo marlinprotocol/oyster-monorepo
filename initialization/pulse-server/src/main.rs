a610cbb67fc2df4f9e22f7c18a9dccd853b2689b
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-12-16 15:37:35 +0530

    basic axum server

--- initialization/pulse-server/src/main.rs
+++ initialization/pulse-server/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Context;
+use anyhow::Result;
+use axum::routing::get;
+use axum::Router;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new().route("/", get(|| async { "Hello" }));
+
+    let listener = TcpListener::bind("0.0.0.0:3000")
+        .await
+        .context("failed to bind listener")?;
+
+    axum::serve(listener, app).await.context("serve error")
 }
