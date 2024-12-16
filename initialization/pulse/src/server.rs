66a9a4ef2e33fb46c0f4f1b70914a740361840cf
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-12-16 17:14:50 +0530

    implement file storage

--- initialization/pulse/src/server.rs
+++ initialization/pulse/src/server.rs
@@ -1,8 +1,9 @@
-use std::convert::Infallible;
+use std::future::Future;
 
 use anyhow::anyhow;
 use anyhow::Context;
 use anyhow::Result;
+use http_body_util::BodyExt;
 use hyper::body::Incoming;
 use hyper::service::service_fn;
 use hyper::Request;
@@ -10,33 +11,43 @@ use hyper::Response;
 use hyper_util::rt::TokioIo;
 use oyster::scallop::new_server_async_Noise_IX_25519_ChaChaPoly_BLAKE2b;
 use tokio::fs::read;
+use tokio::fs::write;
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
 
-async fn hello(_req: Request<Incoming>) -> Result<Response<String>, Infallible> {
-    Ok(Response::new("Hello World!".into()))
-}
-
-async fn handler(stream: TcpStream, secret: [u8; 32]) -> Result<()> {
+async fn handler(stream: TcpStream, secret: [u8; 32], filepath: &str) -> Result<()> {
     let stream =
         new_server_async_Noise_IX_25519_ChaChaPoly_BLAKE2b(stream, &secret, None::<()>, None::<()>)
             .await
             .context("failed to wrap stream with scallop")?;
     let stream = TokioIo::new(stream);
 
-    if let Err(http_err) = hyper::server::conn::http1::Builder::new()
+    hyper::server::conn::http1::Builder::new()
         .keep_alive(true)
-        .serve_connection(stream, service_fn(hello))
+        .serve_connection(
+            stream,
+            service_fn(|req: Request<Incoming>| async move {
+                // store in filepath
+                let body = req
+                    .into_body()
+                    .collect()
+                    .await
+                    .context("failed to collect body")?
+                    .to_bytes();
+                write(filepath, body)
+                    .await
+                    .map(|_| Response::new("success".to_owned()))
+                    .context("failed to write to file")
+            }),
+        )
         .await
-    {
-        eprintln!("Error while serving HTTP connection: {}", http_err);
-    }
-
-    Ok(())
+        .context("failed to serve conn")
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let filepath = "/app/.env";
+
     let secret = read("/app/id.sec")
         .await
         .context("failed to read secret")?
@@ -50,7 +61,7 @@ async fn main() -> Result<()> {
 
     loop {
         let (stream, _) = listener.accept().await?;
-        let res = handler(stream, secret).await;
+        let res = handler(stream, secret, filepath).await;
 
         // exit after first successful transfer
         if res.is_ok() {
