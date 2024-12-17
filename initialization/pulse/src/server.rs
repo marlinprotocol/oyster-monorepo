636e1d5db7934ab8a1a0314a88d0dc6b06c35042
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-12-17 11:19:09 +0530

    include an auther that fetches attestations from a url

--- initialization/pulse/src/server.rs
+++ initialization/pulse/src/server.rs
@@ -1,4 +1,4 @@
-use std::future::Future;
+use std::ops::Deref;
 
 use anyhow::anyhow;
 use anyhow::Context;
@@ -10,16 +10,38 @@ use hyper::Request;
 use hyper::Response;
 use hyper_util::rt::TokioIo;
 use oyster::scallop::new_server_async_Noise_IX_25519_ChaChaPoly_BLAKE2b;
+use oyster::scallop::ScallopAuther;
 use tokio::fs::read;
 use tokio::fs::write;
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
 
-async fn handler(stream: TcpStream, secret: [u8; 32], filepath: &str) -> Result<()> {
-    let stream =
-        new_server_async_Noise_IX_25519_ChaChaPoly_BLAKE2b(stream, &secret, None::<()>, None::<()>)
-            .await
-            .context("failed to wrap stream with scallop")?;
+struct Auther {
+    url: String,
+}
+
+impl ScallopAuther for Auther {
+    type Error = reqwest::Error;
+    async fn new_auth(&mut self) -> Result<Box<[u8]>, Self::Error> {
+        let body = reqwest::get(&self.url).await?.bytes().await?;
+        Ok(body.deref().into())
+    }
+}
+
+async fn handler(
+    stream: TcpStream,
+    secret: [u8; 32],
+    filepath: &str,
+    auther: &mut Auther,
+) -> Result<()> {
+    let stream = new_server_async_Noise_IX_25519_ChaChaPoly_BLAKE2b(
+        stream,
+        &secret,
+        None::<()>,
+        Some(auther),
+    )
+    .await
+    .context("failed to wrap stream with scallop")?;
     let stream = TokioIo::new(stream);
 
     hyper::server::conn::http1::Builder::new()
@@ -46,6 +68,8 @@ async fn handler(stream: TcpStream, secret: [u8; 32], filepath: &str) -> Result<
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let auther_url = "http://127.0.0.1:1300".to_owned();
+    let mut auther = Auther { url: auther_url };
     let filepath = "/app/.env";
 
     let secret = read("/app/id.sec")
@@ -60,8 +84,11 @@ async fn main() -> Result<()> {
         .context("failed to bind listener")?;
 
     loop {
-        let (stream, _) = listener.accept().await?;
-        let res = handler(stream, secret, filepath).await;
+        let (stream, _) = listener
+            .accept()
+            .await
+            .context("failed to accept connections")?;
+        let res = handler(stream, secret, filepath, &mut auther).await;
 
         // exit after first successful transfer
         if res.is_ok() {
