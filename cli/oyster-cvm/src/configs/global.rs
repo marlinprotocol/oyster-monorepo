fbc1929997433f883126abb953b7e4ee64b6381f
arnav-deep <arnavd4@gmail.com>
2025-03-10 17:29:18 +0530

    feat: add credit integration for deploy and deposit

--- cli/oyster-cvm/src/configs/global.rs
+++ cli/oyster-cvm/src/configs/global.rs
@@ -1,6 +1,11 @@
 // Oyster Market Contract Addresses
 pub const OYSTER_MARKET_ADDRESS: &str = "0x9d95D61eA056721E358BC49fE995caBF3B86A34B";
+
+// USDC Contract Address
 pub const USDC_ADDRESS: &str = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831";
+
+// Credit Contract Address
+pub const CREDIT_ADDRESS: &str = "0x972e62BE92e1b29c20a85A47042DaBC556Aa5474";
 
 // RPC URLs
 pub const ARBITRUM_ONE_RPC_URL: &str = "https://arb1.arbitrum.io/rpc";
