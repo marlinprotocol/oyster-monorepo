845b192017ff2f46145e944823afcd7751dbbde3
Ayush-Yadav <ayushya014@gmail.com>
2025-05-02 14:22:35 +0530

    minor serverless code contract fix

--- contracts/contracts/contracts/serverless-v2/OysterServerlessCodeContract.sol
+++ contracts/contracts/contracts/serverless-v2/OysterServerlessCodeContract.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 contract OysterServerlessCodeContract {
 
-    event CodeCreated(bytes indexed metadata);
+    event CodeCreated(bytes metadata);
 
     function saveCodeInCallData(string calldata inputData, bytes calldata metadata) external {
         emit CodeCreated(metadata);
