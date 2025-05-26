46f39f75823db966d3a8050cf7f48c2586b2a4b1
sagarparker <sagar8parker@gmail.com>
2025-05-26 19:02:18 +0530

    Indexer endpoint update

--- cli/oyster-serverless/src/configs/global.rs
+++ cli/oyster-serverless/src/configs/global.rs
@@ -9,4 +9,5 @@ pub const USDC_ADDRESS: &str = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831";
 // RPC URLs
 pub const ARBITRUM_ONE_RPC_URL: &str = "https://arb1.arbitrum.io/rpc";
 
-pub const INDEXER_URL: &str = "https://indexer.oyster.marlin.org/serverless/graphql";
+pub const INDEXER_URL: &str = "https://serverless-indexer.marlin.org/graphql";
+
