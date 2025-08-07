90ca760907fd11e38fb1a585bac0ee9ce324f6af
Souvik Mishra <mishra.souvik911@gmail.com>
2025-08-07 20:01:50 +0530

    add proposal created handler and setup testing

---
+++ contracts/indexer/governance/src/handlers/mod.rs
@@ -0,0 +1,85 @@
+use alloy::rpc::types::Log;
+use anyhow::anyhow;
+use anyhow::Result;
+use diesel::PgConnection;
+use ethp::event;
+use tracing::warn;
+use tracing::{info, instrument};
+
+use crate::LogsProvider;
+
+mod proposal_created;
+use proposal_created::handle_proposal_created;
+
+// proposal logs
+static PROPOSAL_CREATED: [u8; 32] = event!(
+    "ProposalCreated(bytes32,address,uint256,address[],uint256[],bytes[],string,string,(uint256,uint256,uint256,uint256))"
+);
+static PROPOSAL_EXECUTED: [u8; 32] = event!("ProposalExecuted(bytes32)");
+
+// deposit logs
+static DEPOSIT_LOCKED: [u8; 32] = event!("DepositLocked(bytes32,address,uint256)");
+static DEPOSIT_REFUNDED: [u8; 32] = event!("DepositRefunded(bytes32,address,uint256)");
+static DEPOSIT_SLASHED: [u8; 32] = event!("DepositSlashed(bytes32,address,uint256)");
+
+// vote logs
+static VOTE_SUBMITTED: [u8; 32] = event!("VoteSubmitted(bytes32,uint256,address,bytes)");
+
+// ignored logs
+static UPGRADED: [u8; 32] = event!("Upgraded(address)");
+static INITIALIZED: [u8; 32] = event!("Initialized(uint8)");
+static TOKEN_LOCK_AMOUNT_SET: [u8; 32] = event!("TokenLockAmountSet(address,uint256)");
+static NETWORK_CONFIG_SET: [u8; 32] = event!("NetworkConfigSet(uint256,address,string[])");
+static ROLE_GRANTED: [u8; 32] = event!("RoleGranted(bytes32,address,address)");
+static TREASURY_SET: [u8; 32] = event!("TreasurySet(address)");
+static PROPOSAL_PASS_VETO_THRESHOLD_SET: [u8; 32] = event!("ProposalPassVetoThresholdSet(uint256)");
+static MIN_QUORUM_THRESHOLD_SET: [u8; 32] = event!("MinQuorumThresholdSet(uint256)");
+static VOTE_ACTIVATION_DELAY_SET: [u8; 32] = event!("VoteActivationDelaySet(uint256)");
+static VOTE_DURATION_SET: [u8; 32] = event!("VoteDurationSet(uint256)");
+static PROPOSAL_DURATION_SET: [u8; 32] = event!("ProposalDurationSet(uint256)");
+static MAX_RPC_URLS_PER_CHAIN_SET: [u8; 32] = event!("MaxRpcUrlsPerChainSet(uint256)");
+static PCR_CONFIG_SET: [u8; 32] = event!("PCRConfigSet(bytes32,bytes,bytes,bytes)");
+static KMS_ROOT_SERVER_PUB_KEY_SET: [u8; 32] = event!("KMSRootServerPubKeySet(bytes32)");
+static KMS_PATH_SET: [u8; 32] = event!("KMSPathSet(string)");
+
+#[instrument(
+    level = "info",
+    skip_all,
+    parent = None,
+    fields(block = log.block_number, idx = log.log_index, tx = ?log.transaction_hash)
+)]
+pub fn handle_log(conn: &mut PgConnection, log: Log, provider: &impl LogsProvider) -> Result<()> {
+    info!(?log, "processing");
+
+    let log_type = log
+        .topic0()
+        .ok_or(anyhow!("log does not have topic0, should never happen"))?;
+
+    if log_type == PROPOSAL_CREATED {
+        handle_proposal_created(conn, log)
+    } else if log_type == UPGRADED
+        || log_type == INITIALIZED
+        || log_type == TOKEN_LOCK_AMOUNT_SET
+        || log_type == NETWORK_CONFIG_SET
+        || log_type == ROLE_GRANTED
+        || log_type == TREASURY_SET
+        || log_type == PROPOSAL_PASS_VETO_THRESHOLD_SET
+        || log_type == MIN_QUORUM_THRESHOLD_SET
+        || log_type == VOTE_ACTIVATION_DELAY_SET
+        || log_type == VOTE_DURATION_SET
+        || log_type == PROPOSAL_DURATION_SET
+        || log_type == MAX_RPC_URLS_PER_CHAIN_SET
+        || log_type == PCR_CONFIG_SET
+        || log_type == KMS_ROOT_SERVER_PUB_KEY_SET
+        || log_type == KMS_PATH_SET
+    {
+        info!(?log_type, "ignoring log type");
+        Ok(())
+    } else {
+        warn!(?log_type, "unknown log type");
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test_db;
---
+++ contracts/indexer/governance/src/handlers/proposal_created.rs
@@ -0,0 +1,111 @@
+use std::str::FromStr;
+
+use crate::schema::proposals;
+use alloy::hex::ToHexExt;
+use alloy::primitives::Address;
+use alloy::primitives::Bytes;
+use alloy::primitives::U256;
+use alloy::rpc::types::Log;
+use alloy::sol_types::SolValue;
+use anyhow::anyhow;
+use anyhow::Context;
+use anyhow::Result;
+use bigdecimal::BigDecimal;
+use diesel::ExpressionMethods;
+use diesel::PgConnection;
+use diesel::RunQueryDsl;
+use tracing::warn;
+use tracing::{info, instrument};
+
+#[instrument(level = "info", skip_all, parent = None, fields(block = log.block_number, idx = log.log_index))]
+pub fn handle_proposal_created(conn: &mut PgConnection, log: Log) -> Result<()> {
+    info!(?log, "processing proposal created");
+
+    let proposal_id = log.topics()[1].encode_hex_with_prefix();
+    let proposer = Address::from_word(log.topics()[2]).to_checksum(None);
+
+    let tx_hash = log
+        .transaction_hash
+        .ok_or(anyhow!("did not get tx hash from log"))?
+        .encode_hex_with_prefix();
+
+    let (
+        nonce,
+        targets,
+        values,
+        calldatas,
+        title,
+        description,
+        (start_timestamp, vote_activation_timestamp, vote_deadline, proposal_deadline),
+    ) = <(
+        U256,
+        Vec<Address>,
+        Vec<U256>,
+        Vec<Bytes>,
+        String,
+        String,
+        (U256, U256, U256, U256),
+    )>::abi_decode_sequence(&log.data().data, true)?;
+    let (nonce, start_timestamp, proposal_deadline, vote_activation_timestamp, vote_deadline) = (
+        BigDecimal::from_str(&nonce.to_string())?,
+        BigDecimal::from_str(&start_timestamp.to_string())?,
+        BigDecimal::from_str(&proposal_deadline.to_string())?,
+        BigDecimal::from_str(&vote_activation_timestamp.to_string())?,
+        BigDecimal::from_str(&vote_deadline.to_string())?,
+    );
+
+    info!(
+        ?proposal_id,
+        ?proposer,
+        ?nonce,
+        ?targets,
+        ?values,
+        ?calldatas,
+        ?title,
+        ?description,
+        ?start_timestamp,
+        ?vote_activation_timestamp,
+        ?vote_deadline,
+        ?proposal_deadline,
+        ?tx_hash,
+        "creating proposal"
+    );
+
+    // Target SQL:
+    // INSERT INTO proposals (id, proposer, nonce, title, description, tx_hash, executed, proposal_created_at, proposal_end_time, voting_start_time, voting_end_time)
+    // VALUES ("<id>", "<proposer>", "<nonce>", "<title>", "<description>", "<tx_hash>", "<executed>", "<start_timestamp>", "<proposal_deadline>", "<vote_activation_timestamp>", "<vote_deadline>");
+    diesel::insert_into(proposals::table)
+        .values((
+            proposals::id.eq(&proposal_id),
+            proposals::proposer.eq(&proposer),
+            proposals::nonce.eq(&nonce),
+            proposals::title.eq(&title),
+            proposals::description.eq(&description),
+            proposals::tx_hash.eq(&tx_hash),
+            proposals::executed.eq(false),
+            proposals::proposal_created_at.eq(&start_timestamp),
+            proposals::proposal_end_time.eq(&proposal_deadline),
+            proposals::voting_start_time.eq(&vote_activation_timestamp),
+            proposals::voting_end_time.eq(&vote_deadline),
+        ))
+        .execute(conn)
+        .context("failed to create proposal")?;
+
+    info!(
+        ?proposal_id,
+        ?proposer,
+        ?nonce,
+        ?targets,
+        ?values,
+        ?calldatas,
+        ?title,
+        ?description,
+        ?start_timestamp,
+        ?vote_activation_timestamp,
+        ?vote_deadline,
+        ?proposal_deadline,
+        "proposal created"
+    );
+
+    Ok(())
+}
