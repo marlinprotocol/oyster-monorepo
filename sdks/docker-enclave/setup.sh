48f6bd2c4b27316aedf70a0b9105df0c0c8631b9
sagarparker <sagar8parker@gmail.com>
2025-10-08 08:11:35 +0000

    testing

--- sdks/docker-enclave/setup.sh
+++ sdks/docker-enclave/setup.sh
@@ -92,12 +92,12 @@ echo "status"
 /app/supervisord ctl -c /etc/supervisord.conf start attestation-server
 # /app/supervisord ctl -c /etc/supervisord.conf start attestation-server-ecdsa
 
-# echo "Checking connectivity to NFS server with telnet"
-(echo quit) | telnet 172.31.8.34 2049
+# # echo "Checking connectivity to NFS server with telnet"
+# (echo quit) | telnet 172.31.8.34 2049
 
 
 echo "Mounting NFS to /app/nfs/"
-mount -vvv -t nfs -o nolock,vers=4 0.0.0.0:/root/nfs_test/ /app/nfs/
+mount -vvv -t nfs -o nolock,vers=4 172.31.8.34:/root/nfs_test/ /app/nfs/
 if [ $? -eq 0 ]; then
   df -h
   cat /app/nfs/test_file.txt
