829790c318435d0bc6dc53025a09af827643126e
sagarparker <sagar8parker@gmail.com>
2025-10-08 08:37:51 +0000

    testing

--- sdks/docker-enclave/setup.sh
+++ sdks/docker-enclave/setup.sh
@@ -93,7 +93,7 @@ echo "status"
 # /app/supervisord ctl -c /etc/supervisord.conf start attestation-server-ecdsa
 
 # echo "Checking connectivity to NFS server with telnet"
-(echo quit) | telnet 0.0.0.0 2049
+(echo quit) | telnet 0.0.0.0 5555
 
 echo "Mounting NFS to /app/nfs/"
 mount -vvv -t nfs -o nolock,vers=4 0.0.0.0:/home/ubuntu/nfs/general /app/nfs/
