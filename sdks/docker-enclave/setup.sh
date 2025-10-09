38f9d9130a58011ed314168c04a91507b65c7dbc
sagarparker <sagar8parker@gmail.com>
2025-10-09 08:43:12 +0000

    testing

--- sdks/docker-enclave/setup.sh
+++ sdks/docker-enclave/setup.sh
@@ -96,7 +96,7 @@ echo "status"
 telnet 3.111.219.88 2049
 
 echo "Mounting NFS to /app/nfs/"
-mount -vvv -t nfs -o nolock,vers=4 127.0.0.1:/home/ubuntu/nfs/general /app/nfs/
+mount -vvv -t nfs -o nolock,vers=4 3.111.219.88:/home/ubuntu/nfs/general /app/nfs/
 if [ $? -eq 0 ]; then
   df -h
   cat /app/nfs/test_file.txt
