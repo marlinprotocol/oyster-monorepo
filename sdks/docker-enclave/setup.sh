1240dfd9a2e4191da299d8346fd665f7ba49709b
sagarparker <sagar8parker@gmail.com>
2025-10-13 06:22:50 +0000

    testing

--- sdks/docker-enclave/setup.sh
+++ sdks/docker-enclave/setup.sh
@@ -101,7 +101,7 @@ sleep 10
 echo quit | telnet 3.111.219.88 2049
 
 echo "Mounting NFS to /app/nfs/"
-mount -vvv -t nfs -o nolock,vers=4 3.111.219.88:/home/ubuntu/nfs_test /app/nfs/
+mount -vvv -t nfsv4 -o nolock,vers=4 3.111.219.88:/home/ubuntu/nfs_test /app/nfs/
 cat /app/nfs/test_file.txt
 
 sleep 2
