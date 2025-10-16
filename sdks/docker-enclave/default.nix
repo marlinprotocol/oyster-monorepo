112486912ca2720006ba942c46804a7ecc2bd99a
sagarparker <sagar8parker@gmail.com>
2025-10-16 11:35:17 +0000

    testing

--- sdks/docker-enclave/default.nix
+++ sdks/docker-enclave/default.nix
@@ -11,6 +11,7 @@
   derive-server,
   init-params-decoder,
   kernels,
+  pcr-utils,
   compose ? ./. + "/docker-compose.yml",
   dockerImages ? [],
 }: let
@@ -28,6 +29,8 @@
   deriveServer = "${derive-server}/bin/kms-derive-server";
   initParamsDecoder = "${init-params-decoder}/bin/init-params-decoder";
   vet' = "${vet}/bin/vet";
+  pcrExtender = "${pcr-utils}/bin/pcr-extender";
+  pcrLocker = "${pcr-utils}/bin/pcr-locker";
   kernel = kernels.kernel;
   kernelConfig = kernels.kernelConfig;
   nsmKo = kernels.nsmKo;
@@ -76,8 +79,14 @@ in {
     kernel = kernel;
     kernelConfig = kernelConfig;
     nsmKo = nsmKo;
-    cmdline = builtins.readFile nitro.blobs.${eifArch}.cmdLine;
-
+    cmdline = (
+      if eifArch == "aarch64" then
+        "reboot=k panic=30 pci=off nomodules console=ttyS0 random.trust_cpu=on"
+      else if eifArch == "x86_64" then
+        builtins.readFile nitro.blobs.${eifArch}.cmdLine
+      else
+        abort "Unsupported architecture '${eifArch}'"
+      );
     entrypoint = "/app/setup.sh";
     env = "";
     copyToRoot = pkgs.buildEnv {
