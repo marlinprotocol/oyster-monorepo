3bfdb21717d4cf1facdf5c1eec7f680ac5db6119
Akshay Meher <akshay_111meher@hotmail.com>
2025-10-30 11:10:51 +0000

    arb sepolia deployment

--- contracts/contracts-foundry/script/governance/deploy/DeployMockERC20.s.sol
+++ contracts/contracts-foundry/script/governance/deploy/DeployMockERC20.s.sol
@@ -8,13 +8,16 @@ import {console} from "forge-std/console.sol";
 
 contract DeployMockERC20 is Script {
     // Marlin Goverance Token, MGT: Arbitrum Sepolia, Ethereum Sepolia
-    // Deposlit Token: DET: Arbitrum Sepolia
-    function run(string calldata _name, string calldata _symbol) external returns (address) {
+    // Deposit Token: DET: Arbitrum Sepolia
+    function run() external returns (address) {
+        string memory _name = "Marlin Governance Tokens";
+        string memory _symbol = "MGT";
+
         address mockErc20 = deployMockERC20(_name, _symbol);
         return mockErc20;
     }
 
-    function deployMockERC20(string calldata _name, string calldata _symbol) public returns (address) {
+    function deployMockERC20(string memory _name, string memory _symbol) public returns (address) {
         vm.startBroadcast();
         
         // Deploy Implementation
@@ -27,15 +30,16 @@ contract DeployMockERC20 is Script {
         MockERC20(address(proxy)).initialize(
             _name,
             _symbol,
-            0x7C046645E21B811780Cf420021E6701A9E66935C, // Admin
-            0x7C046645E21B811780Cf420021E6701A9E66935C, // Minter Role
-            0x7C046645E21B811780Cf420021E6701A9E66935C  // Burner Role
+            0x7E82Da6A7D4f9Bcc01372e8Fe2E882e18fAd9C5A, // Admin
+            0x7E82Da6A7D4f9Bcc01372e8Fe2E882e18fAd9C5A, // Minter Role
+            0x7E82Da6A7D4f9Bcc01372e8Fe2E882e18fAd9C5A  // Burner Role
         );
 
-        MockERC20(address(proxy)).grantMinterRole(0xF2F8cCc2294748729D3d609c329A3F2c83517Ad5);
+        MockERC20(address(proxy)).grantMinterRole(0x7E82Da6A7D4f9Bcc01372e8Fe2E882e18fAd9C5A);
 
         vm.stopBroadcast();
 
+        console.log("Token: ", _name);
         console.log("MockERC20 deployed at:", address(mockERC20));
         console.log("MockERC20 Proxy deployed at:", address(proxy));
 
