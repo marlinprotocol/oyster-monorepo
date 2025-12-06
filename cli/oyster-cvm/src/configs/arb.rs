cf44f087e3e9b0a91f27ca44f220715b406919fa
Roshan Raghupathy <git@roshanrags.com>
2025-12-06 22:22:43 +0530

    use a different rpc

--- cli/oyster-cvm/src/configs/arb.rs
+++ cli/oyster-cvm/src/configs/arb.rs
@@ -3,4 +3,4 @@ pub const OYSTER_MARKET_ADDRESS: &str = "0x9d95D61eA056721E358BC49fE995caBF3B86A
 pub const USDC_ADDRESS: &str = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831";
 
 // RPC URLs
-pub const ARBITRUM_ONE_RPC_URL: &str = "https://arb1.arbitrum.io/rpc";
+pub const ARBITRUM_ONE_RPC_URL: &str = "https://1rpc.io/arb";
