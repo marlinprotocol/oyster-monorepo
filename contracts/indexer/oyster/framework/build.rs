3c9cd01816c87273afdb73c5fefd84ebd78678d3
Souvik Mishra <mishra.souvik911@gmail.com>
2025-12-15 17:00:25 +0530

    remove warning for schema not updated

--- contracts/indexer/oyster/framework/build.rs
+++ contracts/indexer/oyster/framework/build.rs
@@ -1,7 +1,3 @@
 fn main() {
     println!("cargo:rerun-if-changed=migrations");
-    println!(
-        "cargo:warning=Database migrations changed. \
-        Please ensure that src/schema.rs is updated accordingly!"
-    );
 }
